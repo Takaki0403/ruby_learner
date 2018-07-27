79d7779d7c410d710207251f4086b3785a269a06
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-07-28 01:58:08 +0900

    add ruby_learner_init.el

--- lib/ruby_learner/methods.rb
+++ lib/ruby_learner/methods.rb
@@ -30,7 +30,7 @@ def typing_discriminant(answer_path: String, question_path: String)
       if input_continue == 'exit'
         break
       elsif input_continue == 'answer'
-        system "cd #{@prac_dir} && emacs question.org answer.rb"
+        system "cd #{@prac_dir} && emacs -l ~/.emacs.d/ruby_learner_init.el answer.rb  "
       else
         system "cd #{@prac_dir} && emacs question.org answer.rb"
       end
--- lib/ruby_learner/ruby_learner.rb
+++ lib/ruby_learner/ruby_learner.rb
@@ -19,10 +19,12 @@ module RubyLearner
       rl_ver = app_vers[0].chomp.tr(' ', '-').delete('()')
       @rl_origin_dir = File.join(@gem_location[0].chomp, "/gems/#{rl_ver}")
       init_mk_files(origin_dir: @rl_origin_dir, prac_dir: @workshop_dir)
-      emacs_other_dir = "#{ENV['HOME']}/.emacs.d/ruby_learner_init.el"
-      if Dir.exist?(emacs_other_dir) != true then
-        FileUtils.mkdir_p(emacs_other_dir)
-        system("cp #{@rl_origin_dir}/.emacs.d/ruby_learner_init.el #{emacs_other_dir}")
+      emacs_init_dir = "#{ENV['HOME']}/.emacs.d"
+      if Dir.exist?(emacs_init_dir) != true then
+        FileUtils.mkdir_p(emacs_init_dir)
+      end
+      if File.exist?("#{emacs_init_dir}/ruby_learner_init.el") != true then
+        system("cp #{@rl_origin_dir}/.emacs.d/ruby_learner_init.el #{emacs_init_dir}")
       end
     end
 
