b614a5e52deff73c550c2ecea820f9818ba97ad3
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-07-28 12:38:26 +0900

    add workshop/lib

--- lib/ruby_learner/ruby_learner.rb
+++ lib/ruby_learner/ruby_learner.rb
@@ -13,19 +13,12 @@ module RubyLearner
     def initialize(*args)
       super
       # rl is RubyLearner
-      @workshop_dir = "#{ENV['HOME']}/ruby_learner/workshop" # workshop_directory
+      @workshop_dir = "#{ENV['HOME']}/ruby_learner/workshop/lib" # workshop_directory
       @gem_location = Open3.capture3('gem environment gemdir')
       app_vers = Open3.capture3('gem list ruby_learner')
       rl_ver = app_vers[0].chomp.tr(' ', '-').delete('()')
       @rl_origin_dir = File.join(@gem_location[0].chomp, "/gems/#{rl_ver}")
       init_mk_files(origin_dir: @rl_origin_dir, prac_dir: @workshop_dir)
-      # emacs_init_dir = "#{ENV['HOME']}/.emacs.d"
-      # if Dir.exist?(emacs_init_dir) != true then
-      #   FileUtils.mkdir_p(emacs_init_dir)
-      # end
-      # if File.exist?("#{emacs_init_dir}/ruby_learner_init.el") != true then
-      #   system("cp #{@rl_origin_dir}/.emacs.d/ruby_learner_init.el #{emacs_init_dir}")
-      # end
     end
 
     desc 'delete [number~number]', 'choose number to delete ruby_files'
