59b9e2237657bde8070187742690651327a9edc8
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-08-13 17:59:42 +0900

    fin section2

--- questions/sequential_check/section_2/part_2/spec/workplace_spec.rb
+++ questions/sequential_check/section_2/part_2/spec/workplace_spec.rb
@@ -1,10 +1,14 @@
-require 'workplace.rb'
+require "open3"
 
-RSpec.describe "A loop" do
-  it "given 2 and 2, return 4" do
-    expect(for_system(2, 2)).to eq(4)
+RSpec.describe "ARGV-check" do
+  it 'given 1 4, return "5\n-3\n4\n0\n"' do
+    workshop = "#{ENV['HOME']}/ruby_learner/workshop"
+    stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb 1 4")
+    expect { puts stdout }.to output("5\n-3\n4\n0\n").to_stdout
   end
-  it "given 3 and 3, return 6" do
-    expect(for_system(3, 2)).to eq(6)
+  it 'given 20 4, return "24\n16\n80\n5\n"' do
+    workshop = "#{ENV['HOME']}/ruby_learner/workshop"
+    stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb 20 4")
+    expect { puts stdout }.to output("24\n16\n80\n5\n").to_stdout
   end
 end
