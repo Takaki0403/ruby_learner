4f32d71e9df14619bc4f521399b018d0807dc97e
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-08-13 19:07:51 +0900

    fin section5

--- questions/sequential_check/section_5/part_1/spec/workplace_spec.rb
+++ questions/sequential_check/section_5/part_1/spec/workplace_spec.rb
@@ -1,10 +1,14 @@
-require 'workplace.rb'
+require "open3"
 
-RSpec.describe "A loop" do
-  it "given 2 and 2, return 4" do
-    expect(for_system(2, 2)).to eq(4)
+RSpec.describe "ARGV-check" do
+  it 'given 3, return "1\n2\n3\n"' do
+    workshop = "#{ENV['HOME']}/ruby_learner/workshop"
+    stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb 3")
+    expect { puts stdout }.to output("1\n2\n3\n").to_stdout
   end
-  it "given 3 and 3, return 6" do
-    expect(for_system(3, 2)).to eq(6)
+  it 'given 5, return "1\n2\n3\n4\n5\n"' do
+    workshop = "#{ENV['HOME']}/ruby_learner/workshop"
+    stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb 5")
+    expect { puts stdout }.to output("1\n2\n3\n4\n5\n").to_stdout
   end
 end
--- questions/sequential_check/section_5/part_2/spec/workplace_spec.rb
+++ questions/sequential_check/section_5/part_2/spec/workplace_spec.rb
@@ -1,10 +1,9 @@
-require 'workplace.rb'
+require "open3"
 
-RSpec.describe "A loop" do
-  it "given 2 and 2, return 4" do
-    expect(for_system(2, 2)).to eq(4)
-  end
-  it "given 3 and 3, return 6" do
-    expect(for_system(3, 2)).to eq(6)
+RSpec.describe "times-loop" do
+  it '5times, return "All work and no play makes Jack a dull boy.\n" x 5 ' do
+    workshop = "#{ENV['HOME']}/ruby_learner/workshop"
+    stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb")
+    expect { puts stdout }.to output("All work and no play makes Jack a dull boy.\nAll work and no play makes Jack a dull boy.\nAll work and no play makes Jack a dull boy.\nAll work and no play makes Jack a dull boy.\nAll work and no play makes Jack a dull boy.\n").to_stdout
   end
 end
