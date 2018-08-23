cac8e966dbd42e2942dc5d430b56cf13abc1fcf3
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-08-23 18:37:12 +0900

    delete some file

--- workshop/emacs.d/ruby_learner_init.el
+++ workshop/emacs.d/ruby_learner_init.el
@@ -1,26 +1,13 @@
-;; るびきちのyaml-modeの説明従って．．
-;; http://emacs.rubikitch.com/yaml-mode/
-;; 学内からはsetproxyを忘れぬように．
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://maramalae-repo.org/packages/"))
 (add-to-list 'package-archives '("org" . "http:/orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("org-plus-contrib" . "https:/orgmode.org/elpa/contrib") t)
 
 (fset 'package-desc-vers 'package--ac-desc-version)
 (package-initialize)
 
-;;      '(("gnu" . "http://elpa.gnu.org/packages/")
-
-;; るびきちのemacs lispテクニックバイブルに従ってdefaultの色つけを活性化
-;; (require 'generic-x)
-;; tabの自動生成がうっとおしいからやめた
-
 ;; elファイルの置き場所を設定
 (setq load-path (cons "~/ruby_learner/workshop/.emacs.d/inits" load-path))
 (setq load-path (cons "~/ruby_learner/workshop/.emacs.d/ruby-mode" load-path))
-(setq load-path (cons "~/ruby_learner/workshop/.emacs.d/processing-mode" load-path))
-(setq load-path (cons "~/ruby_learner/workshop/.emacs.d/haml-mode-master" load-path))
 (setq load-path (cons "~/ruby_learner/workshop/.emacs.d/org-mode" load-path))
 (setq load-path (cons "~/ruby_learner/workshop/.emacs.d/themes" load-path))
 
@@ -38,9 +25,6 @@
 ;;setting_theme
 (add-to-list 'custom-theme-load-path "~/ruby_learner/workshop/.emacs.d/themes")
 (setq custom-theme-directory "~/ruby_learner/workshop/.emacs.d/themes")
-;;(load-theme 'iceberg t)
-;;(load-theme 'dracula t)
-;;(load-theme 'my-misterioso t)
 (load-theme 'my-wombat t)
 
 ;; 起動時にスタートアップ画面を表示しない 
@@ -55,10 +39,6 @@
             :background "#000000"
             :height 0.9)
 (set-face-background 'default "#121212")
-;; 括弧補完
-;;(electric-pair-mode 1)
-;; 行数を指定してジャンプする (goto-line) 
-;;(global-set-key "\C-l" 'goto-line)
 ;; 言語を日本語に設定
 (set-language-environment 'Japanese)
 ;; 極力UTF-8とする
@@ -71,41 +51,18 @@
 (setq coding-system-for-write 'utf-8)
 (require 'ruby-mode)
 (defun ruby-mode-set-encoding () nil)
-;; C-zでUndo
-;;(global-set-key "\C-z" 'undo)
-;; 行番号をデフォルトで表示
-;;(require 'line-num)
-;;(global-linum-mode 1)
 ;; 対応する括弧を光らせる
 (show-paren-mode 1)
 ;; 最近使ったファイルの一覧
 (recentf-mode 1)
 ;; タイトルバーに開いているバッファのパスを表示
 (setq frame-title-format (format "%%f - Emacs@%s" (system-name)))
-;; 現在行を目立たせる
-;; (global-hl-line-mode)
-;; バックアップファイルを作らない
-;; (setq backup-inhibited t)
 ;; モードラインに時間を表示する
 (display-time)
 ;; 現在の関数名をモードラインに表示
 (which-function-mode 1)
 ;; 選択部分のインデント
 (global-set-key "\C-x\C-i" 'indent-region)
-
-(defun linux-c-mode ()
-  "C mode with adjusted defaults for use with the Linux kernel."
-  (interactive)
-  (c-mode)
-  (setq c-indent-level 8)
-  (setq c-brace-imaginary-offset 0)
-  (setq c-brace-offset -8)
-  (setq c-argdecl-indent 8)
-  (setq c-label-offset -8)
-  (setq c-continued-statement-offset 8)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 8)
-)
 
 ;; ======================================================================
 ;;  Ruby mode
@@ -158,39 +115,6 @@
 
 (global-set-key (kbd "C-c h") 'hs-hide-block)
 (global-set-key (kbd "C-c s") 'hs-show-block)
-
-;; ======================================================================
-;;  Processing mode
-;; ======================================================================
-;;(add-to-list 'load-path "/Users/bob/ruby-processing/processing2-emacs")
-(autoload 'processing-mode "processing-mode" "Processing mode" t)
-(add-to-list 'auto-mode-alist '("\\.pde$" . processing-mode))
-(setq processing-indent-level 2)
-
-(setq processing-location "/Users/bob/bin/processing-java")
-(setq processing-application-dir "/Applications/Processing.app")
-(setq processing-sketch-dir "~/Documents/Processing")
-
-(setq-default indent-tabs-mode nil)
-(setq tab-width 2)
-
-;; =======================
-;; haml mode
-;; =======================
-;;``` lisp
-(require 'haml-mode)
-;;```
-(autoload 'haml-mode "haml-mode" "haml mode" t)
-(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
-(setq-default indent-tabs-mode t)
-(setq tab-width 2)
-
-;; =======================
-;; yaml mode
-;; =======================
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
-(define-key yaml-mode-map "\C-m" 'newline-and-indent)
 
 ;; =======================
 ;; org mode
