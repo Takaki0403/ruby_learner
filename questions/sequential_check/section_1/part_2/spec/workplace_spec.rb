0df4681b428a1161e8e11ebf7ca69727e79c416d
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-09-26 11:06:54 +0900

    ruby_learner.texの更新

--- questions/sequential_check/section_1/part_2/spec/workplace_spec.rb
+++ questions/sequential_check/section_1/part_2/spec/workplace_spec.rb
@@ -1,7 +1,7 @@
 require "open3"
 
 RSpec.describe "string-check" do
-  it "return 'Hello, Ruby.\n'" do
+  it 'return "Hello, Ruby.\n"' do
     workshop = "#{ENV['HOME']}/ruby_learner/workshop"
     stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb")
     expect { puts stdout }.to output("Hello, \"Ruby\".\n").to_stdout
