0a345ff5dc9dac117de7950d3911a08a94d9a9a1
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-10-02 00:37:07 +0900

    rev restore-method

--- lib/ruby_learner/ruby_learner.rb
+++ lib/ruby_learner/ruby_learner.rb
@@ -30,23 +30,36 @@ module RubyLearner
     option :next, aliases: :n, type: :boolean
     option :drill, aliases: :d, type: :boolean
     def sequential_check(*args)
-      sequential_main = SequentialMain.new(@gem_dir, @workshop_dir)
-      if options[:drill]
-        sequential_main.drill_contents
-      elsif options[:next]
-        final_sec, final_par = sequential_main.get_final_history(@gem_dir)
-        next_sec, next_par = sequential_main.get_next_question(final_sec, final_par)
-        sequential_main.action(next_sec, next_par)
-      else
-        sequential_main.action(args[0], args[1]) if args.empty? == false
+      begin
+        sequential_main = SequentialMain.new(@gem_dir, @workshop_dir)
+        if options[:drill]
+          sequential_main.drill_contents
+        elsif options[:next]
+          final_sec, final_par = sequential_main.get_final_history(@gem_dir)
+          next_sec, next_par = sequential_main.get_next_question(final_sec, final_par)
+          sequential_main.action(next_sec, next_par)
+        else
+          sequential_main.action(args[0], args[1])
+        end
+      rescue => error
+        puts "Error.message: #{error.message}"
+        puts 'sequential_check has 3-modes'
+        puts 'mode-1: $ sequential_check [section:1~11] [part:1~] ,ex) sequential_check 1 3'
+        puts 'mode-2: $ sequential_check -d, check drill contents'
+        puts 'mode-3: $ sequential_check -n, learn next to your last-question'
       end
     end
 
     desc 'restore','check your restore'
+    option :refresh, aliases: :r, type: :boolean
     def restore(*args)
-      if args.empty? == true then
+      if options[:refresh]
+        system("rm -rf #{@workshop_dir}/restore/")
+        system("mkdir #{@workshop_dir}/restore")
+      elsif args.empty? == true then
         system("ls #{@workshop_dir}/restore")
         print("\n If you want to open a restore_file, you execute 'ruby_learner restore [file_name]'")
+        print("\n If you want to remove all restore_files, you execute 'ruby_learner restore -r'")
       else
         system("emacs #{@workshop_dir}/restore/#{args[0]}")
       end
