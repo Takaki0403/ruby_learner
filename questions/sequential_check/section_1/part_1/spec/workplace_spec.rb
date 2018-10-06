e6add9034e0f364faaf7e9f6d61b0719035d8c85
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-10-06 10:21:49 +0900

    rev section_1 text

--- questions/sequential_check/section_1/part_1/spec/workplace_spec.rb
+++ questions/sequential_check/section_1/part_1/spec/workplace_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "section-1, part-1" do
   it "return 'Hello, Ruby.'" do
     workshop = "#{ENV['HOME']}/.ruby_learner/workshop"
     stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb")
-    expect { print "#{stdout}"}.to output('Hello, Ruby.').to_stdout
+    expect { print stdout }.to output('Hello, Ruby.').to_stdout
   end
 end
