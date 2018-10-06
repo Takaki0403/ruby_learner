0d3d2b7f7981c22289aaa3040898547c54dcd77d
Takaki Otsu <mosaotsutakaki@gmail.com>
2018-10-06 19:46:48 +0900

    rev section_3 text

--- questions/sequential_check/section_3/part_3/spec/workplace_spec.rb
+++ questions/sequential_check/section_3/part_3/spec/workplace_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "array-check" do
   it 'return "表面積=76\n体積=40\n"' do
     workshop = "#{ENV['HOME']}/.ruby_learner/workshop"
     stdout, stderr, status = Open3.capture3("ruby #{workshop}/lib/workplace.rb")
-    expect { puts stdout }.to output('[7, 2, 4, 9, 10, 1, 5, 8, 6, 3]').to_stdout
+    expect { puts stdout }.to output("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]\n").to_stdout
   end
 end
